ec80576e1fa4bce359835960a2a0b1d2f59ccbb4
mavaladezt <mavaladezt@gmail.com>
2019-11-02 14:24:43 -0400

    added more graphs

--- 01_loading_dataset.R
+++ 01_loading_dataset.R
@@ -48,3 +48,41 @@ boxplot(SalePrice~Street,data=train,
         main="Street vs Sale Price",xlab="Street",ylab="Sale Price",col="orange",border="brown")
 #it looks like pavement has a better price but certainly has way more variation and outliers
 
+#Alley ####===============================================================================
+#Alley: Type of alley access
+boxplot(SalePrice~Alley,data=train,
+        main="Alley vs Sale Price",xlab="Alley",ylab="Sale Price",col="orange",border="brown")
+#it this variable can help a little bit determine the value of the house
+
+
+#LotShape ####===============================================================================
+#LotShape: General shape of property
+g <- ggplot(data = train, aes(x = reorder(LotShape, SalePrice), y = SalePrice))
+g + geom_boxplot()
+#Have different values but doesn't looks like it moves the needle a lot because of the outliers in the data
+
+#LandCountour ####===============================================================================
+#LandContour: Flatness of the property
+g <- ggplot(data = train, aes(x = reorder(LandContour, SalePrice), y = SalePrice))
+g + geom_boxplot()
+#Can help. only one has too much variability, the rest seem under control
+
+#Utilities ####===============================================================================
+#Utilities: Type of utilities available
+g <- ggplot(data = train, aes(x = reorder(Utilities, SalePrice), y = SalePrice))
+g + geom_boxplot()
+#It might help predict only a few cases
+
+#LotConfig ####===============================================================================
+#LotConfig: Lot configuration
+g <- ggplot(data = train, aes(x = reorder(LotConfig, SalePrice), y = SalePrice))
+g + geom_boxplot()
+#Most of their averages seem like the same
+
+#LandSlope ####===============================================================================
+#LandSlope: Slope of property
+g <- ggplot(data = train, aes(x = reorder(LandSlope, SalePrice), y = SalePrice))
+g + geom_boxplot()
+#Doesn't look like it provides a lot of information
+
+
