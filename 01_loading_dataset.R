aebd5860d83a161ebf40d30c797d65441e5ec39f
mavaladezt <mavaladezt@gmail.com>
2019-11-03 19:03:26 -0500

    added python visualization

--- 01_loading_dataset.R
+++ 01_loading_dataset.R
@@ -26,14 +26,14 @@ boxplot(SalePrice~MSSubClass,data=train,
 #maybe group some categories together that have low variability such as 45/85/160/190
 #maybe make 2-3 different groups out of this column
 
-#MSSubClass ####===============================================================================
+#MSZoning ####===============================================================================
 g <- ggplot(data = train, aes(x = reorder(MSZoning, SalePrice), y = SalePrice))
 g + geom_boxplot()
 #It looks like sales price can be affected by this variable but still there are 2 categories (C and RL) with a lot
 # of variability
 
 #LotFrontage ####===============================================================================
-#Linear feet of street connected to property
+    #Linear feet of street connected to property
 plot(train$LotFrontage,train$SalePrice)
 #maybe create ranges of lots such as from 0-25, 25-50, 50-75, etc?
 
@@ -77,7 +77,7 @@ g + geom_boxplot()
 #LotConfig: Lot configuration
 g <- ggplot(data = train, aes(x = reorder(LotConfig, SalePrice), y = SalePrice))
 g + geom_boxplot()
-#Most of their averages seem like the same
+    #Most of their averages seem like the same
 
 #LandSlope ####===============================================================================
 #LandSlope: Slope of property
